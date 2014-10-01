8416233296a32fc9bc539852e822a52661158c76
Dale Corns <drcorns@hotmail.com>
2014-09-30 17:52:00 -0700

    add loopRun and average time to Function timer

--- drcUtilities.js
+++ drcUtilities.js
@@ -7,15 +7,31 @@ if (!drcUtils){
     this.functionToTime = functionToTime;
     this.startTime = 0;
     this.endTime = 0;
+    this.averageTime = 0;
     this.runTime = function (){return this.endTime - this.startTime;};
     this.run = function (){
-      //Use the Date object to record times
+      //Use the Date object to record times in milliseconds
       this.startTime = new Date().getTime();
       this.endTime = (function(){
         functionToTime();
         return new Date().getTime();
       })();
       return this.endTime - this.startTime;
+    };
+    this.loopRun = function(count) {
+      var oneTime = 0;
+      var ttlTime = 0;
+      var lcount = count || 1;
+      for(var i = 0; i < lcount; i++){
+        console.log('test ' + i);
+        oneTime = this.run();
+        ttlTime += oneTime;
+        console.log(oneTime);
+      }
+      var avrTime = ttlTime / lcount;
+      console.log('Average Time '+ avrTime);
+      this.averageTime = avrTime;
+      return avrTime;
     };
   }
 }
